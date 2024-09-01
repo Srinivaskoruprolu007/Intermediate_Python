a4342df53af41e57d9316e50b2596d3da62aaeec
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2024-09-01 10:59:54 +0530

    dictionary is updated

--- Dictionaries/dicts.py
+++ Dictionaries/dicts.py
@@ -81,4 +81,47 @@ for value in myDict.values():
 for key, value in myDict.items():
     print(key, '--> ',value)
 
+"""
+When you want to copy a dictionary, avoid assigning it directly to another variable, 
+as any changes made to the copied dictionary will also affect the original dictionary.
+"""
+# Avoid this
+mydict_cpy = myDict
+mydict_cpy["key"] = "value"
+print(myDict)
+print(mydict_cpy)
 
+# Better approaches
+mydict_cpy = myDict.copy()
+mydict_cpy['new-key'] = 'new-value'
+print(myDict)
+print(mydict_cpy)
+
+mydict_cpy = dict(myDict)
+mydict_cpy['new-key'] = 'values'
+print(myDict)
+print(mydict_cpy)
+
+
+"""
+Rules for working with dictionaries in Python:
+
+1. Keys must be unique and immutable: You can use types like strings, numbers, or tuples as dictionary keys, 
+but you cannot use lists or other dictionaries.
+
+2. Dictionaries are mutable: You can change the values, 
+add new key-value pairs, or remove existing ones after the dictionary has been created.
+
+3. Dictionaries do not maintain order: In Python versions prior to 3.7, dictionaries do not maintain order. 
+Starting with Python 3.7, dictionaries preserve the insertion order.
+
+4. Avoid direct assignment for copying: When copying a dictionary, use methods like `copy()` or `deepcopy()` to avoid unintentional changes to the original dictionary.
+
+5. Keys must be hashable: This means that the keys must have a hash value that remains the same during their lifetime. 
+Common hashable types include integers, strings, and tuples.
+
+6. Dictionary keys can be of different types: While it's possible to have keys of different types within the same dictionary, 
+it's generally good practice to use consistent key types.
+
+7. Use `in` to check for the existence of a key: Instead of manually searching for a key, use the `in` keyword to determine if a key exists in the dictionary.
+"""
